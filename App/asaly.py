f353fea2d161d6fbc7368e97e0d27cb32d5f6e0b
Hayrettin Basaraner <hbasaraner@gmail.com>
2019-02-15 18:15:07 +0300

    "Error: 404" is resolved.

--- App/asaly.py
+++ App/asaly.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
-@app.route('/Web/')
-def hello_world():
-	return 'index'
+@app.route('/', methods=['GET'])
+def index():
+	return render_template('index.html')
+
+if __name__ == "__main__":
+	app.run()
