f1f6610f31ac7a6f8358c1d49a7e99639a770e69
sertac <33147298+sertacyardimci@users.noreply.github.com>
2019-03-19 16:09:31 +0300

    Block sınıfı yazıldı

--- Blockchain/Block.py
+++ Blockchain/Block.py
@@ -1,1 +1,35 @@
-print("Çikolatalar püskevitler")
+import hashlib
+
+
+class Block:
+    def __init__(self, timeStamp, lastHash, hash, data):
+        self.timeStamp = timeStamp
+        self.lastHash = lastHash
+        self.hash = hash
+        self.data = data
+
+    def __str__(self):
+        return """
+        TimeStamp: {timeStamp}
+        Lasthash: {lastHash}
+        Hash: {hash}
+        Data: {data}
+        """.format(timeStamp=self.timeStamp, lastHash=self.lastHash, hash=self.hash, data=self.data)
+
+    @staticmethod
+    def genesis():
+        timeStamp = "MÖ:1234"
+        lastHash = "asaly"
+        data = "Çikolatalar Püskevitler"
+        block = Block(timeStamp, lastHash, "", data)
+        hash = Block.blockHash(block)
+        block.hash = hash
+        return block
+
+    @staticmethod
+    def blockHash(block):
+        return hashlib.sha256((block.timeStamp+block.lastHash+block.data).encode()).hexdigest()
+
+
+block = Block.genesis()
+print(block)
